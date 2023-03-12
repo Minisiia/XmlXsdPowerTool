0668e13a12b49bce5811090486b76b6dcafd2c6c
Minisiia <ar_tik@yahoo.com>
2023-03-12 14:47:14 +0200

    6. Web-services 1. Software setup, web services, XML

--- src/main/java/parsers/sax/Main.java
+++ src/main/java/parsers/sax/Main.java
@@ -1,21 +1,40 @@
 package parsers.sax;
 
-import org.xml.sax.SAXException;
 import entity.PowerTool;
+import org.xml.sax.SAXException;
 import sort_comparator.Sorted;
 import xsl_transformer_from_xml_to_xml.XslTransformer;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+
+/**
+ * Домашнее задание 1. Задание 5
+ * Электроинструменты.
+ * Электроинструменты можно структурировать по следующей схеме:
+ * — Model — название модели;
+ * — Handy — одно- или двуручное;
+ * — Origin — страна производства;
+ * — TC (должно быть несколько) — технические характеристики:
+ *      энергопотребление (низкое, среднее, высокое),
+ *      производительность (в единицах в час),
+ *      возможность автономного функционирования и т. д.;
+ * — Material — материал изготовления.
+ * Корневой элемент назвать PowerTools или Power.
+ * С помощью XSL преобразовать XML-файл в формат XML, при выводе корневым элементом сделать страну производства.
+ * 1. Создать файл XML и соответствующую ему схему XSD.
+ * 2. При разработке XSD использовать простые и комплексные типы, перечисления, шаблоны и предельные значения.
+ * 3. Сгенерировать класс, соответствующий данному описанию.
+ * 4. Создать приложение для разбора XML-документа и инициализации коллекции объектов информацией из XML-файла.
+ * Для разбора использовать SAX, DOM и StAX парсеры. Для сортировки объектов использовать интерфейс Comparator.
+ * 5. Произвести проверку XML-документа с привлечением XSD.
+ * 6. Определить метод, производящий преобразование разработанного XML документа в документ.
+ * Для преобразования данного XML-документа с помощью XSLT-шаблона использован TransformerFactory API
+ */
 
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
--- src/main/java/parsers/stax/Main.java
+++ src/main/java/parsers/stax/Main.java
@@ -4,13 +4,33 @@ import entity.PowerTool;
 import sort_comparator.Sorted;
 import xsl_transformer_from_xml_to_xml.XslTransformer;
 
-import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
 import java.util.List;
+
+/**
+ * Домашнее задание 1. Задание 5
+ * Электроинструменты.
+ * Электроинструменты можно структурировать по следующей схеме:
+ * — Model — название модели;
+ * — Handy — одно- или двуручное;
+ * — Origin — страна производства;
+ * — TC (должно быть несколько) — технические характеристики:
+ *      энергопотребление (низкое, среднее, высокое),
+ *      производительность (в единицах в час),
+ *      возможность автономного функционирования и т. д.;
+ * — Material — материал изготовления.
+ * Корневой элемент назвать PowerTools или Power.
+ * С помощью XSL преобразовать XML-файл в формат XML, при выводе корневым элементом сделать страну производства.
+ * 1. Создать файл XML и соответствующую ему схему XSD.
+ * 2. При разработке XSD использовать простые и комплексные типы, перечисления, шаблоны и предельные значения.
+ * 3. Сгенерировать класс, соответствующий данному описанию.
+ * 4. Создать приложение для разбора XML-документа и инициализации коллекции объектов информацией из XML-файла.
+ * Для разбора использовать SAX, DOM и StAX парсеры. Для сортировки объектов использовать интерфейс Comparator.
+ * 5. Произвести проверку XML-документа с привлечением XSD.
+ * 6. Определить метод, производящий преобразование разработанного XML документа в документ.
+ * Для преобразования данного XML-документа с помощью XSLT-шаблона использован TransformerFactory API
+ */
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
